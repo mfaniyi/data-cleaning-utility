7b8c919a2a8aae63fc8aa5b6ca1824e80d2191d2
Michael Faniyi <michaelfa@ha-shem.com>
2026-08-07 01:48:42 +0100

    Add edge case tests for data cleaner

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,8 +1,14 @@
 def remove_missing_values(data: list[str | None]) -> list[str]:
+    if not isinstance(data, list):
+        raise TypeError("Input data must be a list.")
+
     cleaned_data = []
 
     for item in data:
         if item is not None and item != "":
+            if not isinstance(item, str):
+                raise TypeError("All items must be strings")
+
             cleaned_data.append(item)
 
     return cleaned_data
--- test_data_cleaner.py
+++ test_data_cleaner.py
@@ -10,8 +10,30 @@ def test_remove_missing_values():
 
     result = remove_missing_values(data)
 
-
     assert result == [
         "Michael",
         "Faniyi"        
     ]
+
+
+def test_empty_list():
+    assert remove_missing_values([]) == []
+
+
+def test_all_missing_values():
+    data = ["", None, ""]
+    assert remove_missing_values(data) == []
+
+
+def test_invalid_input_type():
+    try:
+        remove_missing_values("Michael")
+    except TypeError:
+        assert True
+
+
+def test_invalid_item_type():
+    try:
+        remove_missing_values(["Michael", 123])
+    except TypeError:
+        assert True
